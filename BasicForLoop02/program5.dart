4c0d96a81862de66678bb8d46184fca85047927e
shubhamtarate72 <162182215+shubhamtarate72@users.noreply.github.com>
2024-09-21 13:18:54 +0530

    Delete BasicForLoop02 directory

--- BasicForLoop02/program5.dart
+++
@@ -1,8 +0,0 @@
-
-void main(){
-	for(int i=1;i<=100;i++){
-		if(i%4==0 && i%3==0){
-			print(i);
-		}
-	}
-}
--- BasicForLoop02/program9.dart
+++
@@ -1,8 +0,0 @@
-
-void main(){
-	int sum=0;
-	for(int i=1;i<=15;i++){
-		sum+=i*i;
-	}
-	print(sum);
-}	
