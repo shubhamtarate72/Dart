26d72cb36d853c887ae766e3ce5d8d33257e1e6d
shubhamtarate72 <162182215+shubhamtarate72@users.noreply.github.com>
2024-09-21 13:22:26 +0530

    Delete String_Interpolation directory

--- String_Interpolation/program1.dart
+++
@@ -1,4 +0,0 @@
-void main(){
-	int empId=20;
-	print(empId);
-}
--- String_Interpolation/program3.dart
+++
@@ -1,7 +0,0 @@
-void main(){
-	int empId=20;
-	print("My employee ID is $empId");
-	
-	String firstName=("Incubators");
-	print("My name is $firstName");
-}
